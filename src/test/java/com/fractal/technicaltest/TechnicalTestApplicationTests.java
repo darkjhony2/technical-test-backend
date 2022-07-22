ecb5947d5645edf419189ac78b70730386c6f048
Johnny Quezada <jhonnyquezada1997@gmail.com>
2022-07-22 14:55:28 -0500

    Initial commit

---
+++ src/test/java/com/fractal/technicaltest/TechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fractal.technicaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
